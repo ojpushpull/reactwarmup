b8ded670684ed558c4598c501d14c9b854bdc5fc
Okera Johnson <okerajohnson@gmail.com>
2024-01-03 16:30:51 -0500

    okay green sir

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const list = [
+const stories = [
 {
   title: 'React',
   url: 'https://reactjs.org/',
@@ -32,35 +32,77 @@ function getTitle(title) {
   return title;
 }
 
-function App() {
-  
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Daniel, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+    ];
   return (
     <div>
-    <h1>{list.map}</h1>
-    <h1> Hello {getTitle('React')}</h1>
-    <h1>
-      {welcome.map} {welcome.title}
-    </h1>
+    <h1> React im BACK</h1>
+   
     
-
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" />
-    <ul>
-      {list.map(function (item){
-        return (
-        <li key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            </li>
-        );
-      })}
-    </ul>
+    <Search />
+    
+    
+    <hr />
+    <List list={stories} />
     </div>
     );
+  };
+  
+
+const Search = () => {
+    //
+  const handleChange = (event) => {
+    // synthetic event
+    console.log(event);
+    // value of target (here: input HTML element)
+    console.log(event.target.value);
   }
+
+    return (
+    <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" onChange={handleChange} />
+    </div>
+  );
+};
+
+
+const List = (props) => (
+  <ul>
+      {props.list.map((item) => (
+        <Item key={item.objectID} item={item} />
+      ))}
+    </ul>
+);
+
+        const Item = (props) => (
+          <li>
+          <span>
+            <a href={props.item.url}>{props.item.title}</a>
+          </span>
+            <span>{props.item.author}</span>
+            <span>{props.item.num_comments}</span>
+            <span>{props.item.points}</span>
+            </li>
+        
+    );
+    
 
 export default App;
