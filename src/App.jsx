b9c5b4b56976c5483144396c441d7253e15576fa
Okera Johnson <okerajohnson@gmail.com>
2024-01-10 20:06:31 -0500

    fragmentation is what crystals do

--- src/App.jsx
+++ src/App.jsx
@@ -85,13 +85,16 @@ const App = () => {
   
 
 const Search = ({ search, onSearch }) => (
-  
-  
-    <div>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" value={search} onChange={onSearch} />
-    </div>
-  )
+  <React.Fragment>
+    <label htmlFor="search"> Search:</label>,
+    <input 
+    id="search" 
+    type="text" 
+    value={search} 
+    onChange={onSearch} 
+    />,
+    </React.Fragment>
+);
 
 
 
