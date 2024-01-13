bf510ebf165689fd9d8ea988f693d1d6729e0488
Okera Johnson <okerajohnson@gmail.com>
2024-01-13 10:23:53 -0500

    imperative programming practice

--- src/App.jsx
+++ src/App.jsx
@@ -76,10 +76,12 @@ const App = () => {
     {/* // B */}
     <InputWithLabel
     id="search" 
-    label="Search"
-    value={searchTerm} 
+    value={searchTerm}
+    isFocused 
     onInputChange={handleSearch} 
-    />
+    >
+     <strong> Search: </strong> 
+    </InputWithLabel>
     
     
     <hr />
@@ -89,14 +91,28 @@ const App = () => {
   };
   
 
-const InputWithLabel = ({ id, label, value, type = 'text', onInputChange }) => (
-  <>
-    <label htmlFor={id}> {label} </label>
+const InputWithLabel = ({ id, label, value, type = 'text', onInputChange, isFocused, children, }) => {}
+ //A
+  const inputRef = React.useRef();
+
+  //C 
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      //D
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+ return (
+ <>
+    <label htmlFor={id}> {children} </label>
     &nbsp;
+    {/* B */}
     <input 
+    ref={inputRef}
     id={id} 
     type={type}
-    value={value} 
+    value={value}
+    autoFocus={isFocused}
     onChange={onInputChange} 
     />,
     </>
