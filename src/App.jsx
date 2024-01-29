6796fa7659d5503766f69339a7fac3aa4eaf4b20
Okera Johnson <okerajohnson@gmail.com>
2024-01-29 16:17:43 -0500

    still here according to some theories

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import axios from 'axios';
 
 const initialStories = [
   {
@@ -88,32 +88,35 @@ const App = () => {
       'React'
       );
 
+      const [url, setUrl] = React.useState(
+        `${API_ENDPOINT}${searchTerm}`
+      );
+
     const [stories, dispatchStories] = React.useReducer(
       storiesReducer,
       { data: [], isLoading: false, isError: false }
       );
 
-    const handleFetchStories = React.useCallback(()   =>  {
+    const handleFetchStories = React.useCallback(async ()   =>  {
       //if 'searchterm' is not present
       //e.g. null, empty string undefined
       //do nothing
       //more generalized condition than searchTerm === ''
-      if (!search) return;
+
 
       dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    fetch(`${API_ENDPOINT}${searchTerm}`) //B
-      .then((response) => response.json()) //C
-        .then((result) => {
+      try {
+      const result = await axios.get(url);
+
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.hits,
+          payload: result.data.hits,
         });
-      })
-      .catch(() => 
-        dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
-      );
-     }, [searchTerm]);
+     } catch {
+      dispatchStories({ type: 'STORIES_FETCH_FAILURE'});
+     }
+    }, [url]);
     
      React.useEffect(() => {
       handleFetchStories();
@@ -136,16 +139,41 @@ const App = () => {
       </span>
     );
 
+    const SearchForm = ({
+      searchTerm,
+      onSearchInput,
+      onSearchSubmit,
+    }) => (
+      <form onSubmit={onSearchSubmit}>
+        <InputWithLabel
+          id="search"
+          value={searchTerm}
+          isFocused
+          onInputChange={onSearchInput}
+          >
+            <strong>Search: </strong>
+          </InputWithLabel>
+
+          <button type="submit" disabled={!searchTerm}>
+            Submit
+          </button>
+      </form>
+    );
       
 
 
 
-    const handleSearch = (event) => {
+    const handleSearchInput = (event) => {
     
       // value of target (here: input HTML element)
       setSearchTerm(event.target.value);
       };
 
+    const handleSearchSubmit = (event) => {
+      setUrl(`${API_ENDPOINT}${searchTerm}`);
+
+      event.preventDefault();
+    };
     
 
   
@@ -155,15 +183,11 @@ const App = () => {
    
    
     {/* // B */}
-    <InputWithLabel
-    id="search" 
-    value={searchTerm}
-    isFocused 
-    onInputChange={handleSearch} 
-    >
-     <strong> Search: </strong> 
-    </InputWithLabel>
-    
+   <SearchForm
+   searchTerm={searchTerm}
+   onSearchInput={handleSearchInput}
+   onSearchSubmit={handleSearchSubmit}
+   />
     <ReactInate /> 
     <hr />
 
